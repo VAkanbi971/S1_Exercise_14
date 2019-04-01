7bb8f2dbb010bd62993a92f91431c966e6c99fee
Victor Akanbi <vakanbi971@west-mec.org>
2019-04-01 15:19:20 -0700

    Second Commit
    Added a lot of javascript and added javascript to make css files etc.

--- bc_outline.js
+++ bc_outline.js
@@ -37,4 +37,53 @@ function makeOutline() {
 
       //source document for the outline
       var source = document.getElementById("doc");
+
+      var mainHeading = document.createElement("h1");
+      var outlineList = document.createElement("ol");
+      var headingText = document.createTextNode("Outline");
+
+      mainHeading.appendChild(headingText);
+      outline.appendChild(mainHeading);
+      outline.appendChild(outlineList);
+
+      createList(source, outlineList);
+}
+
+function createList(source, outlineList) {
+      // heading for the outline
+      var headings = ["H1", "H2", "H3", "H4", "H5", "H6"]
+      //previous level of the headings
+      var prevLevel = 0;
+      //running total of article headings
+      var headNum = 0;
+      //this is going to loop through all child nodes of source article till no child nodes are left behind
+      for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+            var headLevel = headings.indexOf(n.nodeName);
+            if (headLevel !== -1) {
+                  //add an id to heading if it is missing
+                  headNum++;
+                  if (n.hasAttribute("id") === false) {
+                        n.setAttribute("id", "head" + headNum);
+                  }
+                  var listElem = document.createElement("li");
+                  listElem.innerHTML = n.firstChild.nodeValue;
+                  if (headLevel === prevLevel) {
+                        //append the list item to the current list
+                        outlineList.appendChild(listElem);
+                  } else if (headLevel > prevLevel) {
+                        //start new nested list
+                        var nestedList = document.createElement("ol");
+                        nestedList.appendChild(listElem);
+                        //append the nested lsit to the last item in the current list
+                        outlineList.lastChild.appendChild(nestedList);
+                        //change current list to the nested list
+                        outlineList = nestedList;
+                  } else {
+                        //append list item to higher list
+                  }
+
+                  //update the value of prevLevel
+                  prevLevel = headLevel;
+            }
+      }
 }
--- bc_switch.js
+++ bc_switch.js
@@ -16,3 +16,73 @@
    view and page view
    
 */
+window.addEventListener("load", setupStyles);
+
+function setupStyles() {
+    //create link element for the page veiw styles
+    var pageStyle = document.createElement("link");
+    pageStyle.setAttribute("href", "bc_page.css");
+    pageStyle.setAttribute("rel", "stylesheet");
+    pageStyle.setAttribute("disabled", "disabled");
+
+    //append link elelement to the document head
+    document.head.appendChild(pageStyle);
+    pageStyle.disabled = true;
+
+    //insert buttons for the style switcher
+    var buttonDIV = document.createElement("div");
+    buttonDIV.setAttribute("id", "styleButtons");
+
+    var webButton = document.createElement("input");
+    webButton.setAttribute("type", "button");
+    webButton.setAttribute("value", "Web View");
+
+    var pageButton = document.createElement("input");
+    pageButton.setAttribute("type", "button");
+    pageButton.setAttribute("value", "Page View");
+
+    buttonDIV.appendChild(webButton);
+    buttonDIV.appendChild(pageButton);
+
+    document.body.insertBefore(buttonDIV, document.body.firstChild);
+
+    //append an embeded stylesheet to the document head
+    var buttonStyles = document.createElement("style");
+    document.head.appendChild(buttonStyles);
+
+    //add style rules to the embed style sheet
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "div#styleButtons { \
+            position:fixed; \
+        }", 0);
+
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "div#styleButtons input{ \
+            background-color: rgba(68, 94, 186, 0.6); \
+            border: 3px solid rgba(0, 24, 123, 0.6); \
+            border-radius: 50%; \
+            cursor: pointer; \
+            color: white; \
+            display: inline-block; \
+            font-size 1.2em; \
+            height: 60px; \
+            margin: 5px 10px; \
+            width: 100px \
+        }", 1);
+
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "@media print{ \
+            div#styleButtons { \
+                 display: none; \
+            } \
+        }", 2);
+
+        //turn the page veiw style off and on
+        webButton.onclick = function() {
+            pageStyle.disabled = true;
+        }
+}
+        pageButton.onclick = function() {
+            pageStyle.disabled = true;
+        }
+}
